c8a38a3950103e7ccc69ab530affbdc45b7e41fd
cgewecke <christophergewecke@gmail.com>
2017-10-08 20:29:15 -0700

    Add gasPrice data fetch, config logic

--- gasStats.js
+++ gasStats.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
 const _ = require('lodash');
-var request = require('request-promise-native');
+const request = require('request-promise-native');
 const shell = require('shelljs');
 const colors = require('colors');
 const gasStatsFile = 'gas-stats.json'
 const Table = require('cli-table2');
-let abiDecoder = require('abi-decoder');
+const reqCwd = require('req-cwd');
+const abiDecoder = require('abi-decoder');
 
+function pretty(msg, obj){
+  console.log(`<------ ${msg} ------>\n` + JSON.stringify(obj, null, ' '));
+  console.log(`<------- END -------->\n`);
+}
 
-function getPrice(data){
+function getPrice(data,){
   return data.price.usd;
 }
 
@@ -16,33 +21,29 @@ function gasToCost(gas, price, gwei){
   return (((gwei * 1e-9) * gas) * price).toFixed(2);
 }
 
-async function generateGasStatsReport(){
-  
-  try {
-    marketData = await request.get('https://coinmarketcap-nexuist.rhcloud.com/api/eth');
-    (!marketData.error)
-      ? price = getPrice(marketData)
-      : price = null;
-  
-  } catch (error) {
-    price = null;
-  }
+async function generateGasStatsReport(methodMap){
+  const {
+    currency,
+    ethPrice,
+    gasPrice
+  } = getGasAndPriceRates();
 
-  let gasStats;
+  const table = new Table({
+    head: [
+      '✜✜✜ GAS STATS ✜✜✜'.bold,
+      'Method',
+      'Min',
+      'Max',
+      'Avg',
+      `${currency} (avg)`
+    ]
+  });
 
-  try {
-    gasStats = JSON.parse(fs.readFileSync(gasStatsFile))
-  } catch (e) {
-    return;
-  }
-
-  var table = new Table({ head: ["✜✜✜ GAS STATS ✜✜✜".bold, 'Method', 'Min', 'Max', 'Avg', 'USD (avg)']});
-  
-  _.forEach(gasStats, (contractData, contractName) => {
+  _.forEach(methodMap, (contractData, contractName) => {
     if(!contractData) return
     let section = {};
-    section[contractName] = [];  
-  
+    section[contractName] = [];
+
     _.forEach(contractData, (fnData, fnName) => {
       fnData.averageGasUsed = fnData.data.reduce((acc, datum) => acc + datum.gasUsed, 0) / fnData.data.length
       const sortedData = _.sortBy(fnData.data, 'gasUsed')
@@ -53,71 +54,75 @@ async function generateGasStatsReport(){
       section[contractName].push(fnData.min.gasUsed);
       section[contractName].push(fnData.max.gasUsed);
       section[contractName].push(fnData.averageGasUsed);
-      section[contractName].push(0); 
+      section[contractName].push(0);
     })
     table.push(section);
-    
+
   })
   shell.rm(gasStatsFile);
   console.log(table.toString());
-  
 }
 
-function pretty(msg, obj){
-  console.log(`<------ ${msg} ------>\n` + JSON.stringify(obj, null, ' '));
-  console.log(`<------- END -------->\n`);
+async function getGasAndPriceRates(){
+  let ethPrice;
+  let gasPrice;
+  const defaultGasPrice = 5000000000;
+
+  // Load config
+  const config = reqCwd.silent('.ethgas.js') || {}
+  const currency = config.currency || 'eur';
+
+  ethPrice = config.ethPrice || null;
+  gasPrice = config.gasPrice || null;
+
+  // Currency market data: coinmarketcap
+  if (!ethPrice){
+    try {
+      const ethPrices = await request.get('https://coinmarketcap-nexuist.rhcloud.com/api/eth');
+      (!ethPrices.error)
+        ? ethPrice = ethPrices.price[currency]
+        : ethPrice = null;
+
+    } catch (error) {
+      ethPrice = null;
+    }
+  }
+
+  // Gas price data: blockcypher
+  if (!gasPrice){
+    try {
+      const gasPrices = await request.get('https://api.blockcypher.com/v1/eth/main');
+      (!gasPrices.error)
+        ? gasPrice = getPrice(marketData)
+        : gasPrice = defaultGasPrice;
+    } catch (error) {
+      gasPrice = defaultGasPrice;
+    }
+  }
+
+  return {
+    currency: currency,
+    ethPrice: ethPrice,
+    gasPrice: gasPrice
+  }
 }
 
-/**
- * Procedure:
- * 1. List all the contracts in the project and artifact.require them into an array;
- * produces an array that looks like:
-    [{
-      name: "MetaCoin"
-      ids: {
-       "7bd703e8": {
-        "constant": false,
-        "inputs": [
-         {
-          "name": "addr",
-          "type": "address"
-         }
-        ],
-        "name": "getBalanceInEth",
-        "outputs": [
-         {
-          "name": "",
-          "type": "uint256"
-         }
-        ],
-        "payable": false,
-        "type": "function"
-       },
-       "90b98a11": {
-        ...etc...
-       },
-       ...etc
-    }]
- *
- */
 function mapMethodsToContracts(truffleArtifacts){
-
   const names = shell.ls('./contracts/**/*.sol');
-  const contractMethodMap = [];
+  const methodMap = {};
   const abis = [];
-  
+
   names.forEach(name => {
     // Get all artifacts, make a list of abi s
     name = name.split('.sol')[0];
     const contract = truffleArtifacts.require(name);
     abis.push(contract._json.abi);
 
-    // Decode, getMethodIDs 
+    // Decode, getMethodIDs
     abiDecoder.addABI(contract._json.abi);
     const methodIDs = abiDecoder.getMethodIDs();
-    
+
     // Create Map;
-    const methodMap = {};
     Object.keys(methodIDs).forEach(key => {
        if (key.name){
          methodMap[key] = {
@@ -127,13 +132,12 @@ function mapMethodsToContracts(truffleArtifacts){
          }
        }
     };
-    
-    // Cleanup
     abiDecoder.removeABI(contract._json.abi);
   };
 
   abis.forEach(abi => abiDecoder.addABI(abi));
-  return contractMethodMap;
+  // TODO: Sort alphabetically by contract name.
+  return methodMap;
 }
 
 function getContractAndMethodName(code){
