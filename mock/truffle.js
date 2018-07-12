cbcdd0493369dbeda311ef2908387aa4adc620eb
c-g-e-w-e-k-e- <christophergewecke@gmail.com>
2018-07-12 07:41:03 -0700

    Revert "Duplicate func"

--- mock/truffle.js
+++ mock/truffle.js
@@ -3,20 +3,10 @@ module.exports = {
     development: {
       host: 'localhost',
       port: 8545,
-      network_id: '*',
-      websockets: true
+      network_id: '*' // Match any network id
     }
   },
   mocha: {
     reporter: 'eth-gas-reporter',
-    reporterOptions: {
-      currency: "chf",
-      gasPrice: 21,
-      onlyCalledMethods: true,
-      noColors: true,
-      rst: true,
-      rstTitle: 'Gas Usage',
-      showTimeSpent: true
-    }
   }
 }
