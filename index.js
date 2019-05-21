2ea15c0c2fedb6290362e5789e535ab12986e920
cgewecke <christophergewecke@gmail.com>
2019-05-21 14:35:12 -0700

    Add prettier / initial lint

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const mocha = require('mocha')
-const inherits = require('util').inherits
-const Base = mocha.reporters.Base
-const color = Base.color
-const log = console.log
-const utils = require('./lib/utils');
-const Config = require('./lib/config');
-const TransactionWatcher = require('./lib/transactionWatcher');
-const GasTable = require('./lib/gasTable');
-const SyncRequest = require('./lib/syncRequest');
+const mocha = require("mocha");
+const inherits = require("util").inherits;
+const Base = mocha.reporters.Base;
+const color = Base.color;
+const log = console.log;
+const utils = require("./lib/utils");
+const Config = require("./lib/config");
+const TransactionWatcher = require("./lib/transactionWatcher");
+const GasTable = require("./lib/gasTable");
+const SyncRequest = require("./lib/syncRequest");
 
 /**
  * Based on the Mocha 'Spec' reporter. Watches an Ethereum test suite run
@@ -20,15 +20,15 @@ const SyncRequest = require('./lib/syncRequest');
  * @param {Object} runner  mocha's runner
  * @param {Object} options reporter.options (see README example usage)
  */
-function Gas (runner, options) {
+function Gas(runner, options) {
   // Spec reporter
   Base.call(this, runner);
   const self = this;
 
-  let indents = 0
-  let n = 0
+  let indents = 0;
+  let n = 0;
   let failed = false;
-  let indent = () => Array(indents).join('  ')
+  let indent = () => Array(indents).join("  ");
 
   // Gas reporter setup
   const config = new Config(options.reporterOptions);
@@ -41,93 +41,94 @@ function Gas (runner, options) {
 
   // ------------------------------------  Runners -------------------------------------------------
 
-  runner.on('start', () => {
-    watch.data.initialize(config)
-  })
+  runner.on("start", () => {
+    watch.data.initialize(config);
+  });
 
-  runner.on('suite', suite => {
-    ++indents
-    log(color('suite', '%s%s'), indent(), suite.title)
-  })
+  runner.on("suite", suite => {
+    ++indents;
+    log(color("suite", "%s%s"), indent(), suite.title);
+  });
 
-  runner.on('suite end', () => {
-    --indents
+  runner.on("suite end", () => {
+    --indents;
     if (indents === 1) {
-      log()
+      log();
     }
-  })
+  });
 
-  runner.on('pending', test => {
-    let fmt = indent() + color('pending', '  - %s')
-    log(fmt, test.title)
-  })
+  runner.on("pending", test => {
+    let fmt = indent() + color("pending", "  - %s");
+    log(fmt, test.title);
+  });
 
-  runner.on('test', () => {
-    watch.beforeStartBlock = sync.blockNumber()
-  })
+  runner.on("test", () => {
+    watch.beforeStartBlock = sync.blockNumber();
+  });
 
-  runner.on('hook end', () => {
-    watch.itStartBlock = sync.blockNumber() + 1
-  })
+  runner.on("hook end", () => {
+    watch.itStartBlock = sync.blockNumber() + 1;
+  });
 
-  runner.on('pass', test => {
-    let fmt
-    let fmtArgs
-    let gasUsedString
-    let consumptionString
-    let timeSpentString = color(test.speed, '%dms')
+  runner.on("pass", test => {
+    let fmt;
+    let fmtArgs;
+    let gasUsedString;
+    let consumptionString;
+    let timeSpentString = color(test.speed, "%dms");
 
     const gasUsed = watch.blocks();
 
     if (gasUsed) {
-      gasUsedString = color('checkmark', '%d gas')
+      gasUsedString = color("checkmark", "%d gas");
 
       if (config.showTimeSpent) {
-        consumptionString = ' (' + timeSpentString + ', ' + gasUsedString + ')'
-        fmtArgs = [test.title, test.duration, gasUsed]
+        consumptionString = " (" + timeSpentString + ", " + gasUsedString + ")";
+        fmtArgs = [test.title, test.duration, gasUsed];
       } else {
-        consumptionString = ' (' + gasUsedString + ')'
-        fmtArgs = [test.title, gasUsed]
+        consumptionString = " (" + gasUsedString + ")";
+        fmtArgs = [test.title, gasUsed];
       }
 
-      fmt = indent() +
-        color('checkmark', '  ' + Base.symbols.ok) +
-        color('pass', ' %s') +
+      fmt =
+        indent() +
+        color("checkmark", "  " + Base.symbols.ok) +
+        color("pass", " %s") +
         consumptionString;
-
     } else {
       if (config.showTimeSpent) {
-        consumptionString = ' (' + timeSpentString + ')'
-        fmtArgs = [test.title, test.duration]
+        consumptionString = " (" + timeSpentString + ")";
+        fmtArgs = [test.title, test.duration];
       } else {
-        consumptionString = ''
-        fmtArgs = [test.title]
+        consumptionString = "";
+        fmtArgs = [test.title];
       }
 
-      fmt = indent() +
-        color('checkmark', '  ' + Base.symbols.ok) +
-        color('pass', ' %s') +
-        consumptionString
+      fmt =
+        indent() +
+        color("checkmark", "  " + Base.symbols.ok) +
+        color("pass", " %s") +
+        consumptionString;
     }
-    log.apply(null, [fmt, ...fmtArgs])
-  })
+    log.apply(null, [fmt, ...fmtArgs]);
+  });
 
-  runner.on('fail', test => {
+  runner.on("fail", test => {
     failed = true;
-    let fmt = indent() + color('fail', '  %d) %s')
-    log()
-    log(fmt, ++n, test.title)
-  })
+    let fmt = indent() + color("fail", "  %d) %s");
+    log();
+    log(fmt, ++n, test.title);
+  });
 
-  runner.on('end', () => {
-    table.generate(watch.data, config)
-    self.epilogue()
+  runner.on("end", () => {
+    table.generate(watch.data, config);
+    self.epilogue();
   });
 }
 
 /**
  * Inherit from `Base.prototype`.
  */
-inherits(Gas, Base)
+inherits(Gas, Base);
 
-module.exports = Gas
+module.exports = Gas;
